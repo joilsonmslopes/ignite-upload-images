00135c8cf580909eb381d8997c98d49ab77e5ad0
joilsonmslopes <joilsonmirandasl@hotmail.com>
2023-07-23 12:27:28 -0300

    chore: add cardList component rules

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,32 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = ({ url }: { url: string }): void => {
+    onOpen();
+    setSelectedImageUrl(url);
+  };
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards?.map(card => (
+          <Card
+            key={card.id}
+            data={card}
+            viewImage={url => handleViewImage({ url })}
+          />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        imgUrl={selectedImageUrl}
+        onClose={onClose}
+      />
     </>
   );
 }

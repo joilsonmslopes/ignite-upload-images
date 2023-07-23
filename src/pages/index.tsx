990c85aa4bb7b204bacc3bca4c5ef1e741bc1a30
joilsonmslopes <joilsonmirandasl@hotmail.com>
2023-07-23 12:27:51 -0300

    chore: add home page rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,30 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+interface GetResponseData {
+  after: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
+  const fetchImages = async ({ pageParam = 0 }): Promise<GetResponseData> => {
+    const { data } = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return data;
+  };
+
   const {
     data,
     isLoading,
@@ -16,20 +39,25 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const newData = data?.pages.flatMap(item => {
+      return item.data.flat();
+    });
+
+    return newData;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +65,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
